ef5fc7c0509e7216e1cc8d7879c88cd7ad015c99
paaban25 <paaban25@gmail.com>
2025-04-12 23:04:11 +0530

    Prediction Model Finalized
    Final Prediction Model

--- CNN_Matlab/randomScript.m
+++ CNN_Matlab/randomScript.m
@@ -1,8 +1,9 @@
-input_image_path = "C:\Users\ASUS\Downloads\cat.jpeg";
+input_image_path = "C:\Users\ASUS\Downloads\doggy.jpeg";
 input_image = imread(input_image_path);
 resized_image = imresize(input_image, [100, 100]);
-filters_conv1 = rand(3, 3, 3, 32);
-filters_bias1 = rand(1, 32);
+imshow(input_image_path)
+% filters_conv1 = rand(3, 3, 3, 32);
+% filters_bias1 = rand(1, 32);
 stride_conv=1;
 max_pool_size=2;
 stride_pooling=2;
@@ -11,8 +12,8 @@ stride_pooling=2;
 % relu1_output=relu_activation(conv1_output);
 % maxPooling1_output=MaxPooling2D_layer(relu1_output,max_pool_size,stride_pooling);
 
-filters_conv2=rand (3,3,32,32);
-filters_bias2=rand(1,32);
+% filters_conv2=rand (3,3,32,32);
+% filters_bias2=rand(1,32);
 
 % conv2_output=conv2D_layer2(maxPooling1_output,filters_conv2,filters_bias2,stride_conv);
 % relu2_output = relu_activation(conv2_output);
@@ -20,14 +21,14 @@ filters_bias2=rand(1,32);
 
 
 % flatten_output=Flatten_layer(maxPooling2_output);
-weights_dense1= rand(64,16928);
-bias_dense1 = rand(64,1);
+% weights_dense1= rand(64,16928);
+% bias_dense1 = rand(64,1);
 % dense1_output=Dense_layer(flatten_output,weights_dense1,bias_dense1,64);
 
 % relu3_output=relu_activation(dense1_output);
 
-weights_dense2=rand(1,64);
-bias_dense2= rand(1,1);
+% weights_dense2=rand(1,64);
+% bias_dense2= rand(1,1);
 % dense2_output=Dense_layer(relu3_output,weights_dense2,bias_dense2,1);
 
 % final_activation_output=sigmoid_activation(dense2_output);
